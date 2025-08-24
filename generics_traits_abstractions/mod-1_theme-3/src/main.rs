daa065fd851695bcee784f6bf65a89b30fb2d901
Yury Bikuzin <yury.bikuzin@gmail.com>
2025-08-24 15:47:19 +0300

    second attempt

--- generics_traits_abstractions/mod-1_theme-3/src/main.rs
+++ generics_traits_abstractions/mod-1_theme-3/src/main.rs
@@ -1,18 +1,15 @@
-use std::sync::Arc;
-
 fn main() {
     println!("Hello, world!");
 }
 
+// ----------------------------
+//
 enum AccountError {
-    AccountIsBlocked,
+    AccountIsAlreadyLocked,
     InsufficientFunds,
 }
 
-// #[derive(Hash, PartialEq, Eq)]
-// struct AccountId(u64);
-
-trait Account<AccountId: std::hash::Hash + PartialEq + Eq> {
+trait Account<AccountId: std::hash::Hash + PartialEq + Eq>: Clone {
     fn id(&self) -> AccountId;
     //
     fn lock(&self) -> Result<(), AccountError>;
@@ -22,61 +19,119 @@ trait Account<AccountId: std::hash::Hash + PartialEq + Eq> {
     fn credit(&self, amount: Amount);
 }
 
-trait Operation<AccountId: std::hash::Hash + PartialEq + Eq> {
-    fn debit_account(&self) -> Option<(Arc<Box<dyn Account<AccountId>>>, Amount)> {
-        None
-    }
-    fn credit_account(&self) -> Option<(Arc<Box<dyn Account<AccountId>>>, Amount)> {
-        None
-    }
-}
-
 #[derive(Clone, Copy)]
 struct Amount(u64);
 
-impl<AccountId: std::hash::Hash + PartialEq + Eq> Operation<AccountId> for Withdrawal<AccountId> {
-    fn debit_account(&self) -> Option<(Arc<Box<dyn Account<AccountId>>>, Amount)> {
-        Some((Arc::clone(&self.account), self.debit))
+// ----------------------------
+
+trait Operation<A, AccountId>
+where
+    A: Account<AccountId>,
+    AccountId: std::hash::Hash + PartialEq + Eq,
+{
+    fn debit_account(&self) -> Option<(&A, Amount)> {
+        None
+    }
+    fn credit_account(&self) -> Option<(&A, Amount)> {
+        None
     }
 }
 
-impl<AccountId: std::hash::Hash + PartialEq + Eq> Operation<AccountId> for Deposit<AccountId> {
-    fn credit_account(&self) -> Option<(Arc<Box<dyn Account<AccountId>>>, Amount)> {
-        Some((Arc::clone(&self.account), self.credit))
+// ----------------------------
+
+struct Withdrawal<A, AccountId>
+where
+    A: Account<AccountId>,
+    AccountId: std::hash::Hash + PartialEq + Eq,
+{
+    account: A,
+    debit: Amount,
+    marker: std::marker::PhantomData<AccountId>,
+}
+
+impl<A, AccountId> Operation<A, AccountId> for Withdrawal<A, AccountId>
+where
+    A: Account<AccountId>,
+    AccountId: std::hash::Hash + PartialEq + Eq,
+{
+    fn debit_account(&self) -> Option<(&A, Amount)> {
+        Some((&self.account, self.debit))
     }
 }
 
-impl<AccountId: std::hash::Hash + PartialEq + Eq> Operation<AccountId> for Transfer<AccountId> {
-    fn debit_account(&self) -> Option<(Arc<Box<dyn Account<AccountId>>>, Amount)> {
-        Some((Arc::clone(&self.withdrawal.account), self.withdrawal.debit))
-    }
-    fn credit_account(&self) -> Option<(Arc<Box<dyn Account<AccountId>>>, Amount)> {
-        Some((Arc::clone(&self.deposit.account), self.deposit.credit))
+struct Deposit<A, AccountId>
+where
+    A: Account<AccountId>,
+    AccountId: std::hash::Hash + PartialEq + Eq,
+{
+    account: A,
+    credit: Amount,
+    marker: std::marker::PhantomData<AccountId>,
+}
+
+impl<A, AccountId> Operation<A, AccountId> for Deposit<A, AccountId>
+where
+    A: Account<AccountId>,
+    AccountId: std::hash::Hash + PartialEq + Eq,
+{
+    fn credit_account(&self) -> Option<(&A, Amount)> {
+        Some((&self.account, self.credit))
     }
 }
 
-struct TransactionError<AccountId: std::hash::Hash + PartialEq + Eq> {
-    account: Arc<Box<dyn Account<AccountId>>>,
+struct Transfer<A, AccountId>
+where
+    A: Account<AccountId>,
+    AccountId: std::hash::Hash + PartialEq + Eq,
+{
+    withdrawal: Withdrawal<A, AccountId>,
+    deposit: Deposit<A, AccountId>,
+}
+
+impl<A, AccountId> Operation<A, AccountId> for Transfer<A, AccountId>
+where
+    A: Account<AccountId>,
+    AccountId: std::hash::Hash + PartialEq + Eq,
+{
+    fn debit_account(&self) -> Option<(&A, Amount)> {
+        self.withdrawal.debit_account()
+    }
+    fn credit_account(&self) -> Option<(&A, Amount)> {
+        self.deposit.credit_account()
+    }
+}
+
+// ----------------------------
+
+struct TransactionError<A, AccountId>
+where
+    A: Account<AccountId>,
+    AccountId: std::hash::Hash + PartialEq + Eq,
+{
+    account: A,
     err: AccountError,
+    marker: std::marker::PhantomData<AccountId>,
 }
 
-trait Transaction<AccountId: std::hash::Hash + PartialEq + Eq> {
-    fn apply(&self) -> Result<(), TransactionError<AccountId>> {
+trait Transaction<A, AccountId>
+where
+    A: Account<AccountId>,
+    AccountId: std::hash::Hash + PartialEq + Eq,
+{
+    fn apply(&self) -> Result<(), TransactionError<A, AccountId>> {
         let debit_accounts = self.debit_accounts();
         let credit_accounts = self.credit_accounts();
         let unique_accounts = {
-            let mut count_of_succeffully_locked_accounts = 0;
             use itertools::Itertools;
-            let unique_accounts = debit_accounts
+            debit_accounts
                 .iter()
-                .map(|(account, _amount)| Arc::clone(&account))
-                .chain(
-                    credit_accounts
-                        .iter()
-                        .map(|(account, _amount)| Arc::clone(&account)),
-                )
+                .map(|(account, _amount)| account)
+                .chain(credit_accounts.iter().map(|(account, _amount)| account))
                 .unique_by(|account| account.id())
-                .collect::<Vec<_>>();
+                .collect::<Vec<_>>()
+        };
+        {
+            let mut count_of_succeffully_locked_accounts = 0;
             for account in unique_accounts.iter() {
                 if let Err(err) = account.lock() {
                     for account in unique_accounts
@@ -86,15 +141,15 @@ trait Transaction<AccountId: std::hash::Hash + PartialEq + Eq> {
                         account.unlock();
                     }
                     return Err(TransactionError {
-                        account: Arc::clone(account),
+                        account: (**account).clone(),
                         err,
+                        marker: std::marker::PhantomData,
                     });
                 } else {
                     count_of_succeffully_locked_accounts += 1;
                 }
             }
-            unique_accounts
-        };
+        }
         {
             let mut count_of_successfully_debited_accounts = 0;
             for (account, amount) in debit_accounts.iter() {
@@ -106,8 +161,9 @@ trait Transaction<AccountId: std::hash::Hash + PartialEq + Eq> {
                         account.credit(*amount);
                     }
                     return Err(TransactionError {
-                        account: Arc::clone(account),
+                        account: (**account).clone(),
                         err,
+                        marker: std::marker::PhantomData,
                     });
                 } else {
                     count_of_successfully_debited_accounts += 1;
@@ -122,60 +178,133 @@ trait Transaction<AccountId: std::hash::Hash + PartialEq + Eq> {
         }
         Ok(())
     }
-    fn debit_accounts(&self) -> Vec<(Arc<Box<dyn Account<AccountId>>>, Amount)>;
-    fn credit_accounts(&self) -> Vec<(Arc<Box<dyn Account<AccountId>>>, Amount)>;
+    fn debit_accounts(&self) -> Vec<(&A, Amount)>;
+    fn credit_accounts(&self) -> Vec<(&A, Amount)>;
 }
 
-// struct SingleOperationTransaction<AccountId: std::hash::Hash + PartialEq + Eq>
-// // <T: Operation<AccountId>>
-// // where
-// //     AccountId: std::hash::Hash + PartialEq + Eq, // <
-// //     // AccountId: Operation<AccountId>,
-// //     AccountId: std::hash::Hash + PartialEq + Eq,
-// //     // AccountId: std::hash::Hash + PartialEq + Eq,
-// // >
-// {
-//     operation: Operation<AccountId>,
-//     // operation: Box<dyn Operation<AccountId>>,
-// }
+// ----------------------------
 
-// struct SingleOperationTransaction<T: Operation<AccountId: std::hash::Hash + PartialEq + Eq>> {
-//     operation: T,
-// }
-//
-struct SingleOperationTransaction<T, AccountId>
+struct SingleOperationTransaction<T, A, AccountId>
 where
-    T: Operation<AccountId>,
+    T: Operation<A, AccountId>,
+    A: Account<AccountId>,
     AccountId: std::hash::Hash + PartialEq + Eq,
 {
     operation: T,
-    marker: std::marker::PhantomData<AccountId>,
+    marker_a: std::marker::PhantomData<A>,
+    marker_account_id: std::marker::PhantomData<AccountId>,
 }
-//
-struct PairedOperationTransaction<T1, T2, AccountId>
+
+impl<T, A, AccountId> Transaction<A, AccountId> for SingleOperationTransaction<T, A, AccountId>
 where
-    T1: Operation<AccountId>,
-    T2: Operation<AccountId>,
+    T: Operation<A, AccountId>,
+    A: Account<AccountId>,
+    AccountId: std::hash::Hash + PartialEq + Eq,
+{
+    fn debit_accounts(&self) -> Vec<(&A, Amount)> {
+        if let Some((account, amount)) = self.operation.debit_account() {
+            vec![(account, amount)]
+        } else {
+            vec![]
+        }
+    }
+    fn credit_accounts(&self) -> Vec<(&A, Amount)> {
+        if let Some((account, amount)) = self.operation.credit_account() {
+            vec![(account, amount)]
+        } else {
+            vec![]
+        }
+    }
+}
+
+// ----------------------------
+
+struct PairedOperationTransaction<T1, T2, A, AccountId>
+where
+    T1: Operation<A, AccountId>,
+    T1: Operation<A, AccountId>,
+    A: Account<AccountId>,
     AccountId: std::hash::Hash + PartialEq + Eq,
 {
     first_operation: T1,
     second_operation: T2,
-    marker: std::marker::PhantomData<AccountId>,
+    marker_a: std::marker::PhantomData<A>,
+    marker_account_id: std::marker::PhantomData<AccountId>,
 }
 
-//
-
-struct Withdrawal<AccountId: std::hash::Hash + PartialEq + Eq> {
-    account: Arc<Box<dyn Account<AccountId>>>,
-    debit: Amount,
+impl<T1, T2, A, AccountId> Transaction<A, AccountId>
+    for PairedOperationTransaction<T1, T2, A, AccountId>
+where
+    T1: Operation<A, AccountId>,
+    T2: Operation<A, AccountId>,
+    A: Account<AccountId>,
+    AccountId: std::hash::Hash + PartialEq + Eq,
+{
+    fn debit_accounts(&self) -> Vec<(&A, Amount)> {
+        vec![
+            self.first_operation.debit_account(),
+            self.second_operation.debit_account(),
+        ]
+        .into_iter()
+        .flatten()
+        .collect::<Vec<_>>()
+    }
+    fn credit_accounts(&self) -> Vec<(&A, Amount)> {
+        vec![
+            self.first_operation.credit_account(),
+            self.second_operation.credit_account(),
+        ]
+        .into_iter()
+        .flatten()
+        .collect::<Vec<_>>()
+    }
 }
 
-struct Deposit<AccountId: std::hash::Hash + PartialEq + Eq> {
-    account: Arc<Box<dyn Account<AccountId>>>,
-    credit: Amount,
+// ----------------------------
+
+struct MultipleOperationTransaction<A, AccountId>
+where
+    A: Account<AccountId>,
+    AccountId: std::hash::Hash + PartialEq + Eq,
+{
+    operations: Vec<Box<dyn Operation<A, AccountId>>>,
+    marker_account_id: std::marker::PhantomData<AccountId>,
 }
 
-struct Transfer<AccountId: std::hash::Hash + PartialEq + Eq> {
-    withdrawal: Withdrawal<AccountId>,
-    deposit: Deposit<AccountId>,
-}
+// #[cfg(test)]
+// mod tests {
+
+// struct SimpleAccount {
+//     id: u64,
+//     is_locked: std::sync::atomic::AtomicBool,
+//     balance: std::sync::atomic::AtomicU64,
+// }
+// impl Account<u64> for std::sync::Arc<SimpleAccount> {
+//     fn id(&self) -> u64 {
+//         self.id
+//     }
+//     fn lock(&self) -> Result<(), AccountError> {
+//         if self
+//             .is_locked
+//             .swap(true, std::sync::atomic::Ordering::SeqCst)
+//         {
+//             Err(AccountError::AccountIsAlreadyLocked)
+//         } else {
+//             Ok(())
+//         }
+//     }
+//     fn unlock(&self) {
+//         self.is_locked
+//             .store(false, std::sync::atomic::Ordering::SeqCst)
+//     }
+//     fn debit(&self, amount: Amount) -> Result<(), AccountError> {
+//         if
+//         self.balance.fetch_add(amount.0)
+//     }
+//     fn credit(&self, amount: Amount) {
+//         self.balance.fetch_add(amount.0)
+//     }
+//     fn get_balance(&self) -> u64 {}
+// }
+
+// }
